747a1fd547e8f032dc8767efc64374ce401edd61
Mosomali <mascuuddaadaa@gmail.com>
2024-03-30 07:02:21 -0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./Home"
+
+import "@fontsource/outfit";
+import "@fontsource/roboto";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Home/>
+  </React.StrictMode>
+);
